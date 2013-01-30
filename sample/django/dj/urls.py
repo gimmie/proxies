a7e69d1612edb68ef6ba2c25df9e9e61c04dd500
Maythee Anegboonlap <natkung@gmail.com>
2013-01-30 18:28:03 +0800

    Update sample to use DjangoProxy.

--- sample/django/dj/urls.py
+++ sample/django/dj/urls.py
@@ -4,10 +4,14 @@ import os.path
 from django.conf.urls import patterns, include, url
 from django.http import HttpResponse
 
+import gimmie
+import django
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+gimmie_proxy = gimmie.DjangoProxy(oauth_key='d883ceb9d02d6b73eae54464075f', oauth_secret='0212f8ad3bd4c900fe3784dae456', url_prefix='http://api.lvh.me:3000', cookie_key='_gm_user', django=django)
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'dj.views.home', name='home'),
@@ -18,7 +22,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-	url(r'^gimmie', 'gimmie.views.proxy'),
+	url(r'^gimmie$', gimmie_proxy),
 	url(r'^$', 'gimmie.views.index'),
 	url(r'^system/', 'gimmie.views.image_from_api'),
 	url(r'^(?P<path>.*)$', 'django.views.static.serve', {
--- sample/django/gimmie/views.py
+++ sample/django/gimmie/views.py
@@ -1,29 +1,11 @@
-import gimmie
-
 from django.http import HttpResponse
 from django.shortcuts import redirect
-
-import logging
-
-logger = logging.getLogger(__name__)
-client = gimmie.Client(oauth_key='d883ceb9d02d6b73eae54464075f', oauth_secret='0212f8ad3bd4c900fe3784dae456', url_prefix='http://api.lvh.me:3000')
 
 # Create your views here.
 def index(request):
 	response = redirect('/index.html')
+	response.set_cookie('_gm_user', 'test')
 	return response
 
 def image_from_api(request):
 	return redirect('http://api.lvh.me:3000%s'%request.get_full_path())
-
-def proxy(request):
-	url_suffix = request.get_full_path().split("gimmieapi=").pop()
-	player_uid = request.get_signed_cookie('_gm_user', False)
-	if not player_uid:
-		player_uid = 'test'
-	
-	url_suffix = request.get_full_path().split("gimmieapi=").pop()
-	status, headers, body = client.get(url_suffix, player_uid)
-	
-	response = HttpResponse(body, content_type='application/json')
-	return response
